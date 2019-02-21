34a1afc474c2c7976587e610bf2f5f7c7d917c56
ryanAltobello <rchap504@gmail.com>
2019-02-21 17:36:35 -0600

    Updates

---
+++ precache-manifest.13f562189a36ec9ca4e8d8d1c64f76b1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecef2d84146236da2d38",
+    "url": "/social-cards-practice/static/js/runtime~main.ecef2d84.js"
+  },
+  {
+    "revision": "55123a002c2a22a94012",
+    "url": "/social-cards-practice/static/js/main.55123a00.chunk.js"
+  },
+  {
+    "revision": "9428879d57d29168f57a",
+    "url": "/social-cards-practice/static/js/2.9428879d.chunk.js"
+  },
+  {
+    "revision": "97224451215b5530e4059c077ee157e4",
+    "url": "/social-cards-practice/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/social-cards-practice/precache-manifest.13f562189a36ec9ca4e8d8d1c64f76b1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/social-cards-practice/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
